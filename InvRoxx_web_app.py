251750a4275faa94474477eff97577af64252cc5
Warpotatoes <109367617+Warpotatoes@users.noreply.github.com>
2024-08-07 19:52:59 +0200

    test os detection

--- InvRoxx_web_app.py
+++ InvRoxx_web_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 image_path='images/'
 
+import os
+
+st.write(os.name)
+
+import platform
+
+st.write(platform.system())
+st.write(platform.release())
+
 ######################
 # Pages
 ######################
